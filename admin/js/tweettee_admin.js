a6d518be3e527b7aea79c7bf0da54ea25c4ef3eb
wladweb <wladwebwork@gmail.com>
2015-03-18 19:56:38 +0300

    bf

--- admin/js/tweettee_admin.js
+++
@@ -1,9 +0,0 @@
-
-jQuery(document).ready(function(){
-    if (window.tweettee_oauth_authorize_url !== undefined){
-        window.location.href = tweettee_oauth_authorize_url;
-    }
-    if (window.tweettee_oauth_redir_url !== undefined){
-        window.location.href = tweettee_oauth_redir_url;
-    }
-});
