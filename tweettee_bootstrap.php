af8cd427a11e5f2ab991372e2ec532b4f51b5c33
wladweb <wladwebwork@gmail.com>
2019-04-06 14:36:50 +0300

    bootstrap file

--- tweettee_bootstrap.php
+++ tweettee_bootstrap.php
@@ -4,37 +4,22 @@
  * Plugin Name:       Tweettee
  * Plugin URI:        
  * Description:       Plugin show user timeline, home timeline, mentions, home timeline another twitter users, search result by post tags, category name, free word. Tweets can be show in widget and also between posts in main loop on home page.
- * Version:           1.0.0
+ * Version:           1.1.0
  * Author:            Vladimir Petrov
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       plugin-name
+ * Text Domain:       tweettee
  * Domain Path:       /languages
  */
 
-namespace Tweettee;
-use Tweettee\Includes as Inc;
-
-if (! defined('WPINC')){
-	die;
+if (!defined('WPINC')) {
+    die;
 }
 
-require_once 'autoload.php';
+require_once 'vendor/autoload.php';
 
-$activate_tweettee = function(){
-    require_once plugin_dir_path(__FILE__) . 'includes/tweettee_activator.php';
-    Inc\Tweettee_Activator::activate();
-};
+register_activation_hook(__FILE__, ['\Wladweb\Tweettee\Includes\TweetteeActivator', 'activate']);
+register_deactivation_hook(__FILE__, ['\Wladweb\Tweettee\Includes\TweetteeDeactivator', 'deactivate']);
 
-$deactivate_tweettee = function(){
-    require_once plugin_dir_path(__FILE__) . 'includes/tweettee_deactivator.php';
-    Inc\Tweettee_Deactivator::deactivate();
-};
-
-register_activation_hook(__FILE__, $activate_tweettee);
-register_deactivation_hook(__FILE__, $deactivate_tweettee);
-
-require_once plugin_dir_path(__FILE__) . 'includes/tweettee.php';
-
-$tweettee = new Inc\Tweettee;
-$tweettee->plugin_start(); 
+$tweettee = new Wladweb\Tweettee\Includes\Core\TweetteeApp;
+$tweettee->plugin_start();
